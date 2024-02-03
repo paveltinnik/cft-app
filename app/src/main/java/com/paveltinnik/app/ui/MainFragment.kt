f1127c0dc3dc899ee510d0d0b90c378cadd276dc
Павел <t280396ab@mail.ru>
2024-02-03 23:42:57 +0700

    fix

--- app/src/main/java/com/paveltinnik/app/ui/MainFragment.kt
+++ app/src/main/java/com/paveltinnik/app/ui/MainFragment.kt
@@ -5,13 +5,16 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AbsListView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.paveltinnik.app.R
 import com.paveltinnik.app.adapter.PersonAdapter
 import com.paveltinnik.app.databinding.FragmentMainBinding
+import com.paveltinnik.app.util.Constants.Companion.QUERY_PAGE_SIZE
 import com.paveltinnik.app.util.Resource
 
 class MainFragment : Fragment() {
@@ -57,7 +60,12 @@ class MainFragment : Fragment() {
                 is Resource.Success -> {
                     hideProgressBar()
                     response.data?.let { personsResponse ->
-                        personAdapter.differ.submitList(personsResponse.results)
+                        personAdapter.differ.submitList(personsResponse.results.toList())
+                        val totalPages = 100 / QUERY_PAGE_SIZE + 2
+                        isLastPage = viewModel.pageNumber == totalPages
+                        if (isLastPage) {
+                            binding.rvResultList.setPadding(0, 0, 0, 0)
+                        }
                     }
                 }
 
@@ -77,10 +85,45 @@ class MainFragment : Fragment() {
 
     private fun hideProgressBar() {
         binding.paginationProgressBar.visibility = View.INVISIBLE
+        isLoading = false
     }
 
     private fun showProgressBar() {
         binding.paginationProgressBar.visibility = View.VISIBLE
+        isLoading = true
+    }
+
+    var isLoading = false
+    var isLastPage = false
+    var isScrolling = false
+
+    val scrollListener = object : RecyclerView.OnScrollListener() {
+        override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+            super.onScrollStateChanged(recyclerView, newState)
+            if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL) {
+                isScrolling = true
+            }
+        }
+
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
+
+            val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+            val firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
+            val visibleItemCount = layoutManager.childCount
+            val totalItemCount = layoutManager.itemCount
+
+            val isNotLoadingAndNotLastPage = !isLoading && !isLastPage
+            val isAtLastItem = firstVisibleItemPosition + visibleItemCount >= totalItemCount
+            val isNotAtBeginning = firstVisibleItemPosition >= 0
+            val isTotalMoreThanVisible = totalItemCount >= QUERY_PAGE_SIZE
+            val shouldPaginate = isNotLoadingAndNotLastPage && isAtLastItem && isNotAtBeginning &&
+                    isTotalMoreThanVisible && isScrolling
+            if (shouldPaginate) {
+                viewModel.getPersons()
+                isScrolling = false
+            }
+        }
     }
 
     override fun onDestroyView() {
@@ -93,6 +136,7 @@ class MainFragment : Fragment() {
         binding.rvResultList.apply {
             adapter = personAdapter
             layoutManager = LinearLayoutManager(activity)
+            addOnScrollListener(this@MainFragment.scrollListener)
         }
     }
 }
--- app/src/main/java/com/paveltinnik/app/util/Constants.kt
+++ app/src/main/java/com/paveltinnik/app/util/Constants.kt
@@ -3,5 +3,6 @@ package com.paveltinnik.app.util
 class Constants {
     companion object {
         const val BASE_URL = "https://randomuser.me/"
+        const val QUERY_PAGE_SIZE = 20
     }
 }
